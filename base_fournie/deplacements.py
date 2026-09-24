03e7d545a3f40cdbf91fc7895b3e132485cebb30
Skander Saafi <skandersaafi52@gmail.com>
2026-09-24 18:03:08 +0200

    fonction de chargement de l'appartement implémentée

--- base_fournie/deplacements.py
+++ base_fournie/deplacements.py
@@ -1,1 +1,19 @@
+import json;
 #Moteur de déplacement
+
+#Chargement de l'appartement pour récuperer sa taille exacte
+def charger_appartement(chemin):
+    with open(chemin, "r", encoding="utf-8") as fichier:
+        appartement = json.loads(fichier);
+    
+    hauteur = appartement["dimentions"]["hauteur"]
+    largeur = appartement["dimensions"]["largeur"]
+    return appartement, hauteur, largeur
+
+
+if __name__ == "__main__":
+    import sys;
+    appartement, hauteur, largeur = charger_appartement(sys.argv[1])
+    print("Hauteur : "+hauteur)
+    print("Largeur : "+largeur)
+
