f6b8878868c12d15a23103b2b3c8b0dcfeef703e
Skander <skander.saafi@etu.umontpellier.fr>
2026-09-25 16:32:17 +0200

    Ajouter la fonction de calcul de chemin entre le robot et les autres points

--- base_fournie/deplacements.py
+++ base_fournie/deplacements.py
@@ -1,5 +1,6 @@
 #Moteur de déplacement
 import json
+from math import *
 
 #Chargement de l'appartement pour récuperer sa taille exacte
 def charger_appartement(chemin):
@@ -17,7 +18,7 @@ def charger_appartement(chemin):
     grille = appartement["grille"]
     return appartement, hauteur, largeur, pos_robot, pos_residents, pos_dict, pos_armoire, grille
 
-
+#Creation de la carte memoire
 def creer_carte_memoire(hauteur, largeur):
     carte = []
     for i in range(hauteur):
@@ -27,6 +28,7 @@ def creer_carte_memoire(hauteur, largeur):
         carte.append(ligne)
     return carte
 
+#Initialisation de la carte memoire
 def initialiser_memoire(carte, pos_robot, pos_residents, pos_dict, pos_armoire):
     carte[pos_robot[0]][pos_robot[1]] = 'R'
     carte[pos_dict[0]][pos_dict[1]] = 'D'
@@ -35,6 +37,7 @@ def initialiser_memoire(carte, pos_robot, pos_residents, pos_dict, pos_armoire):
         carte[pos_resident[0]][pos_resident[1]] = 'P'
     return carte
 
+#Fonction de perception des alentours
 def perception(grille, position):
     ligne = position[0]
     colonne = position[1]
@@ -45,6 +48,7 @@ def perception(grille, position):
     perception["W"] = grille[ligne][colonne-1]
     return perception
 
+#Mise à jour après perception des alentours
 def update_memoire(perception, memoire, position):
     ligne = position[0]
     colonne = position[1]
@@ -53,6 +57,51 @@ def update_memoire(perception, memoire, position):
     memoire[ligne][colonne+1] = perception["E"]
     memoire[ligne][colonne-1] = perception["W"]
     return memoire
+
+def calcul_chemin(carte_memoire, position_depart, position_arrivee):
+    file = [position_depart]
+    parents = {tuple(position_depart) : None}       #Ca on le met pour optimiser la recherche et ne pas tomber sur une case déjà visitée 
+    while file:
+        position = file.pop(0)
+        if position == position_arrivee:
+            break
+        ligne = position[0]
+        colonne = position[1]
+        voisins = [
+            [ligne-1,colonne],
+            [ligne+1,colonne],
+            [ligne,colonne+1],
+            [ligne,colonne-1]
+        ]
+        for voisin in voisins:              #Ici pour chaque voisin on doit s'assurer qu'il est accessible et qu'il n'est pas déjà visité, c'est la qu'on ajoute le voisin à la file pour l'explorer, et on met notre position actuelle dans le tuple parents
+            ligne_voisin = voisin[0]
+            colonne_voisin = voisin[1]
+            if ligne_voisin < 0 or ligne_voisin >= len(carte_memoire):
+                continue
+            if colonne_voisin < 0 or colonne_voisin >= len(carte_memoire[0]):
+                continue
+            if carte_memoire[ligne_voisin][colonne_voisin] == "#":
+                continue
+            voisin_tuple = tuple(voisin)
+            if voisin_tuple in parents:
+                continue
+            parents[voisin_tuple] = position
+            file.append(voisin)
+
+    if tuple(position_arrivee) not in parents:
+        return None
+    position = tuple(position_arrivee)
+    chemin = []
+    while position is not None:
+        chemin.append(list(position))
+        position = parents[position]
+        if position is not None:
+            position = tuple(position)
+    chemin.reverse()
+
+    return chemin
+
+#Tests dans le main
 if __name__ == "__main__":
     import sys
     appartement, hauteur, largeur,pos_robot, pos_residents, pos_dict, pos_armoire, grille = charger_appartement(sys.argv[1])
@@ -67,5 +116,7 @@ if __name__ == "__main__":
     carte_init = initialiser_memoire(carte, pos_robot, pos_residents, pos_dict, pos_armoire)
     print(carte_init)
     print("Perception actuelle : ",perception)
-    print("Memoire mise à jour : ", update_memoire(perception, carte_init, pos_robot))
+    carte_updated = update_memoire(perception, carte_init, pos_robot)
+    print("Memoire mise à jour : ", carte_updated)
+    print(calcul_chemin(carte_updated, pos_robot, pos_dict))
 
